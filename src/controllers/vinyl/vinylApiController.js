9ea43e88b2568fee95bbe11ded3e542d0257033e
Mikel <mikelon_santxez@hotmail.com>
2024-05-06 13:11:02 +0200

    creados controladores de vinilo

---
+++ src/controllers/vinyl/vinylApiController.js
@@ -0,0 +1,52 @@
+import vinylController from "./vinylController.js";
+
+async function getAll(req,res){
+    const {error,data} = await vinylController.getAll();
+    res.json({error,data});
+}
+
+async function getById(req,res){
+    const id = parseInt(req.params.id);
+    console.log("id",id);
+    const{error,data} = await vinylController.getById(id)
+    res.json({error,data});
+}
+
+async function create(req,res){
+    // const {name,is_alive,birth_date} = req.body;
+    const {album_name,artist_name,price,relase_date} = req.query;
+    const {error,data} = await vinylController.create({album_name,artist_name,price,relase_date});
+    res.json({error,data});
+}
+
+async function update(req,res){
+    const id = parseInt(req.params.id);
+    // const {name,is_alive,birth_date} = req.body;
+    const {album_name,artist_name,price,relase_date} = req.query;
+    const {error,data} = await vinylController.update(id,{album_name,artist_name,price,relase_date});
+    res.json({error,data});
+}
+
+async function remove(req,res){
+    const id = parseInt(req.params.id);
+    const {error,data} = await vinylController.remove(id);
+    res.json({error,data});
+}
+
+
+
+export {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
+
+export default {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
