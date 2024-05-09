a5a0ec078e00933a67634ce3f06f654150490aac
Mikelondrio <mikelon_santxez@hotmail.com>
2024-05-09 13:58:21 +0200

    solucion de fallos

--- src/controllers/vinyl/vinylController.js
+++ src/controllers/vinyl/vinylController.js
@@ -1,86 +1,73 @@
-const vinyls = [
-	{
-		"id_vinyl" : 1,
-		"album_name" : "Nevermind",
-		"artist_name" : "Nirvana",
-		"price" : 3,
-        "relase_date" : "03/12/1992"
-	},
-	{
-		"id_vinyl" : 2,
-		"album_name" : "Ensalada de coño",
-		"artist_name" : "Tu madre es puta",
-		"price" : 6,
-        "relase_date" : "26/07/2001"
-	},
-	{
-		"id_vinyl" : 3,
-		"album_name" : "Matadero 5",
-		"artist_name" : "Taburete",
-		"price" : 57,
-        "relase_date" : "11/05/2022"
-	}
-]
-async function getAll(){
-    return {data:vinyls}
+import vinylModel from "../../models/vinylModel.js";
+
+async function getAll() {
+    try {
+        const vinyls = await vinylModel.findAll({include:["vinyls"]});
+        return { data: vinyls };
+    }
+    catch (error) {
+        console.error(error);
+        return { error: error };
+    }
 }
 
-async function getById(id){
-    const vinyl = vinyls.find(vinyl => vinyl.id_vinyl === id);
-    if(!vinyl){
-        return {error:"El vinilo no existe"};
+async function getById(id) {
+    try {
+        const vinyl = await vinylModel.findByPk(id);
+        if (!vinyl) {
+            return { error: "El vinilo no existe" };
+        }
+        return { data: vinyl };
     }
-    return {data:vinyl};
+    catch (error) {
+        console.error(error);
+        return { error };
+    }
+
 }
 
-async function create(vinylData){
-    const {album_name,artist_name,price,relase_date} = vinylData;
-    // get max vinyl_id from vinyls
-    if(!album_name ){
-        return {error:"Los vinilos deben tener nombre!"};
+async function create(vinylData) {
+    try {
+        const newVinyl = await vinylModel.create(vinylData);
+        console.log("new vinyl",newVinyl);
+        return {data:newVinyl};
+    } catch (error) {
+        console.error(error);
+        return {error}
     }
-    const maxId = Math.max(...vinyls.map(vinyl => vinyl.id_vinyl));
-    const newId= maxId + 1;
-    const newVinyl = {
-        id_vinyl:newId,
-        album_name,
-        artist_name,
-        price,
-        relase_date
 
-    };
-    vinyls.push(newVinyl);
-    return {data:newVinyl};
+
 }
 
-async function update(id,VinylData){
-    const {album_name,artist_name,price,relase_date} = VinylData;
-    const vinyl = vinyls.find(vinyl=>vinyl.id_vinyl===id);
-    if(!vinyl){
-        return {error:"No se puede modificar un vinilo que no existe, bobolon"};
+async function update(id, vinylData) {
+    try {
+        if(vinylData.name === ""){
+            delete vinylData.name;
+        }
+        const newVinyl = await vinylModel.update(vinylData,
+            {
+                where: 
+                {
+                    id_vinyl:id
+                }
+            }
+        );
+        return {data:newVinyl};
+    } catch (error) {
+        console.error(error);
+        return {error}
     }
-    if(album_name){
-        vinyl.album_name = album_name;
-    }
-    if(artist_name){
-        vinyl.artist_name=artist_name;
-    }
-    if(price){
-        vinyl.price = price;
-    }
-    if(relase_date){
-        vinyl.relase_date = relase_date;
-    }
-    return {data:vinyl};
+   
 }
 
-async function remove(id){
-    const vinylIndex = vinyls.findIndex(vinyl=>vinyl.id_vinyl===id);
-    if(vinylIndex === -1){
-        return {error:"no se pueden borrar vinilos que no existen"}
+async function remove(id) {
+    try {
+        const result = await vinylModel.remove(id);
+        return {data:result};
+    } catch (error) {
+        console.error(error);
     }
-    const deletedVinyl = vinyls.splice(vinylIndex,1);
-    return {data:deletedVinyl};
+    
 }
 
 export {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import router from "./routes/router.js";
 
 dotenv.config();
