5ac013010a3022b02d7e596672a7e1a1523e1991
cristobendito <flopybbb@gmail.com>
2024-05-14 13:21:33 +0200

    changes

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -33,10 +33,4 @@ const userModel = sequelize.define("user",
 });
  */
 
-/* userModel.hasMany(cartModel, { as: "carritos", foreignKey: 'id_user' }); */
-
-/* //Al consultar un usuario, podrías acceder a su carrito así:
-User.findByPk(1, { include: Carrito }).then(user => {
-    console.log(user.Carrito); });// Objeto del carrito asociado al usuario */
-
 export default userModel;
