8e2187e016dfd313db2ab4a4d04d697ef1df8e38
Mikelondrio <mikelon_santxez@hotmail.com>
2024-05-14 16:09:22 +0200

    inicio de privilegios de admin

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,13 +2,17 @@ import { Router } from "express";
 import { isAdmin } from "../controllers/middlewares/authMiddleware.js";
 import apiRouter from "./apiRoutes/apiRouter.js";
 import viewRouter from "./viewRoutes/viewRouter.js";
+import vinylViewRouter from "./viewRoutes/vinylViewRouter.js"
 
 const router = Router();
 
 
+router.use("/vinyl",isAdmin,vinylViewRouter);
 
 router.use("/api",apiRouter);
-router.use("/", viewRouter);
+router.use("/",viewRouter);
+
+
 
 
 export default router;
