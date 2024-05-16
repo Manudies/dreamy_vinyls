2e2701012770f59fadd76474b22461fd57c9e8f5
Manudies <manudies@gmail.com>
2024-05-16 22:09:44 +0200

    fix: modifico permisos de rutas

--- src/routes/viewRoutes/vinylViewRouter.js
+++ src/routes/viewRoutes/vinylViewRouter.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
 
 import vinylViewController from "../../controllers/vinyl/vinylViewController.js";
-import { isAdmin } from "../../controllers/middlewares/authMiddleware.js";
+import { isAdmin, hasSession } from "../../controllers/middlewares/authMiddleware.js";
 const router = Router();
 
 router.get("/",vinylViewController.getAll);
 router.get("/new",isAdmin,vinylViewController.createForm);
 router.post("/",isAdmin,vinylViewController.create);
-router.get("/:id",isAdmin,vinylViewController.getById);
+router.get("/:id",vinylViewController.getById);
 router.get("/:id/update",isAdmin,vinylViewController.updateForm);
 //router.delete("/:id",vinylViewController.remove);
 router.post("/:id/remove",isAdmin,vinylViewController.remove);
-router.post("/add", vinylViewController.addToCart);
+router.post("/add",hasSession, vinylViewController.addToCart);
 router.post("/:id",isAdmin,vinylViewController.update);
 
 
